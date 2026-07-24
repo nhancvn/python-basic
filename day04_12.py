acf99ebc299ea9554388c2e7d88539892c01fcec
nhancvn <nhanc.intern@vnext.vn>
2026-07-24 18:46:18 +0900

    refactor: improve function and variable names

--- day04_12.py
+++ day04_12.py
@@ -1,111 +1,157 @@
-#bài 1:
-#Đếm xem có bao nhiêu số dương
-def num1 (numbers):
+# bài 1:
+# Đếm xem có bao nhiêu số dương
+def count_positive(numbers):
     count = 0
-    for n in numbers:
-        if n > 0:
+    for number in numbers:
+        if number > 0:
             count += 1
     return count
-numbers = [24, 10, 11, 40, 29, 23]
-print(num1(numbers))
+
+positive_numbers = [24, 10, 11, 40, 29, 23]
+print(count_positive(positive_numbers))
 
 
-#bài 2:
-#Đếm xem có bao nhiêu số âm
-def num2 (numbers):
+# bài 2:
+# Đếm xem có bao nhiêu số âm
+def count_negative(numbers):
     count = 0
-    for n in numbers:
-        if n < 0:
+    for number in numbers:
+        if number < 0:
             count += 1
     return count
-numbers = [11, -12, -10, -24, -29, -40, 26]
-print(num2(numbers))
 
-#bài 3:
-#Tính tổng các số dương 
-def num3 (numbers):
+sample_numbers = [11, -12, -10, -24, -29, -40, 26]
+print(count_negative(sample_numbers))
+
+
+# bài 3:
+# Tính tổng các số dương
+def sum_positive(numbers):
     total = 0
-    for n in numbers:
-        if n > 0:
-            total += n
+    for number in numbers:
+        if number > 0:
+            total += number
     return total
-numbers = [11, -12, -10, -24, -29, -40, 26]
-print(num3(numbers))
 
-#bài 4:
-#Tính tổng các số âm
-def num4 (numbers):
+print(sum_positive(sample_numbers))
+
+
+# bài 4:
+# Tính tổng các số âm
+def sum_negative(numbers):
     total = 0
-    for n in numbers:
-        if n < 0:
-            total += n
-    return total 
-numbers = [11, -12, -10, -24, -29, -40, 26]
-print(num4(numbers))
+    for number in numbers:
+        if number < 0:
+            total += number
+    return total
 
-#bài 5:
-#Tìm vị trí của số lớn nhất
-def num5 (numbers):
-    A = 0
-    for i in range(1, len(numbers)):
-        if numbers[i] > numbers[A]:
-            A = i
-    return A
-numbers = [11, -12, -10, -24, -29, -40, 26]
-print(num5(numbers))
+print(sum_negative(sample_numbers))
 
-#bài 6:
-#Tìm vị trí của số nhỏ nhất
-def num6 (numbers):
-    M = 0
-    for i in range(1, len(numbers)):
-        if numbers[i] < numbers[M]:
-            M = i
-    return M
-numbers = [11, -12, -10, -24, -29, -40, 26]
-print(num6(numbers))
 
-#bài 7:
-#Đổi chỗ số lớn nhất và số nhỏ nhất
-def num7 (numbers):
+# bài 5:
+# Tìm vị trí của số lớn nhất
+def get_index_max(numbers):
+    max_index = 0
+    for index in range(1, len(numbers)):
+        if numbers[index] > numbers[max_index]:
+            max_index = index
+    return max_index
+
+print(get_index_max(sample_numbers))
+
+
+# bài 6:
+# Tìm vị trí của số nhỏ nhất
+def get_index_min(numbers):
+    min_index = 0
+    for index in range(1, len(numbers)):
+        if numbers[index] < numbers[min_index]:
+            min_index = index
+    return min_index
+
+print(get_index_min(sample_numbers))
+
+
+# bài 7:
+# Đổi chỗ số lớn nhất và số nhỏ nhất
+def swap_max_min(numbers):
     if not numbers:
         return numbers
-    A = num5(numbers)
-    M = num6(numbers)
-    numbers[A], numbers[M] = numbers[M], numbers[A]
-    return numbers
-numbers = [11, -12, -10, -24, -29, -40, 26]
-print(num7(numbers))
 
-#bài 8:
-#Xóa phần tử tại một vị trí
-def num8 (numbers, position):
+    max_index = get_index_max(numbers)
+    min_index = get_index_min(numbers)
+
+    numbers[max_index], numbers[min_index] = numbers[min_index], numbers[max_index]
+    return numbers
+
+print(swap_max_min(sample_numbers.copy()))
+
+
+# bài 8:
+# Xóa phần tử tại một vị trí
+def remove_at_position(numbers, position):
     if position < 0 or position >= len(numbers):
         return numbers
+
     numbers.pop(position)
     return numbers
-numbers = [11, -12, -10, -24, -29, -40, 26]
-print(num8(numbers, 1))
 
-#bài 9:
-#Kiểm tra xem list có đối xứng hay ko ?
-def num9 (numbers):
-    n = len(numbers) 
-    for i in range(n // 2):
-        if numbers[i] != numbers[n - 1 - i]:
+print(remove_at_position(sample_numbers.copy(), 1))
+
+
+# bài 9:
+# Kiểm tra xem list có đối xứng hay không
+def is_palindrome(numbers):
+    length = len(numbers)
+
+    for index in range(length // 2):
+        if numbers[index] != numbers[length - 1 - index]:
             return False
-    return True
-print(num9([1,2,4,5,9]))
 
-#bài 10:
-#Dịch trái list một lần
-def num10 (numbers):
+    return True
+
+print(is_palindrome([1, 2, 3, 2, 1]))
+
+
+# bài 10:
+# Dịch trái list một lần
+def rotate_left(numbers):
     if not numbers:
         return numbers
-    first = numbers[0]
+
+    first_value = numbers[0]
     numbers = numbers[1:]
-    numbers.append(first)
+    numbers.append(first_value)
+
     return numbers
-print(num10([1,2,4,5,9]))
+
+print(rotate_left([1, 2, 4, 5, 9]))
 
 
+# bài 11:
+# Kiểm tra tất cả các phần tử có phải số dương hay không
+def all_positive(numbers):
+    if not numbers:
+        return True
+
+    for number in numbers:
+        if number <= 0:
+            return False
+
+    return True
+
+print(all_positive(sample_numbers))
+
+
+# bài 12:
+# Tìm hiệu giữa số lớn nhất và số nhỏ nhất
+def max_min_difference(numbers):
+    if not numbers:
+        return 0
+
+    max_index = get_index_max(numbers)
+    min_index = get_index_min(numbers)
+
+    return numbers[max_index] - numbers[min_index]
+
+print(max_min_difference(sample_numbers))
