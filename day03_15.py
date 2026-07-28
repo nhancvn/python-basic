6c53617ca6a18a1bdc2e9f5d2ff47391b3c42c0f
nhancvn <nhanc.intern@vnext.vn>
2026-07-28 11:30:42 +0900

    refactor: update day03 practice

--- day03_15.py
+++ day03_15.py
@@ -152,7 +152,7 @@ def num10 (numbers):
    for i in range(n):
       for j in range(0, n - i - 1):
          if numbers[j] < numbers[j + 1]:
-             temp = numbers[i]
+             temp = numbers[j]
              numbers[j] = numbers[j + 1]
              numbers[j + 1] = temp
    return numbers
